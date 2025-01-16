106c88b021ebc5b9e0fd0eac3ec62290a892bb40
LSJ0706 <zosksldhs@gmail.com>
2025-01-16 09:47:13 +0900

    feat 07 반복문

--- part1/07.반복문/(lv.1)sum-up-to.js
+++ part1/07.반복문/(lv.1)sum-up-to.js
@@ -11,6 +11,13 @@
  */
 function sumUpTo(n) {
   // TODO: n이 1 이상이면 1부터 n까지의 합을 반환, 0 이하이면 0을 반환
+  let cnt = 0;
+  let sum = 0;
+  while (cnt <= n) {
+    sum += cnt;
+    cnt++;
+  }
+  return sum;
 }
 
 // export를 수정하지 마세요.
