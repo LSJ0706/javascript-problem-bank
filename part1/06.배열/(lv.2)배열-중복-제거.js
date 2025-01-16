7b37950a4069859bf79d21b630c54888c3a75fe5
LSJ0706 <zosksldhs@gmail.com>
2025-01-16 09:34:30 +0900

    feat 06배열

--- part1/06.배열/(lv.2)배열-중복-제거.js
+++ part1/06.배열/(lv.2)배열-중복-제거.js
@@ -10,6 +10,8 @@
  */
 function getUniqueElements(arr) {
   // TODO: 중복을 제거한 새 배열을 반환하세요.
+  const set = new Set(arr);
+  return [...set];
 }
 
 // export를 수정하지 마세요.
--- part1/06.배열/(lv.3)배열-메서드-체이닝.js
+++ part1/06.배열/(lv.3)배열-메서드-체이닝.js
@@ -16,6 +16,10 @@
 
 function get20sEmail(arr) {
   // TODO: 배열 메서드 체이닝을 사용해 20대 사람의 email 배열을 반환하세요.
+  return arr
+    .filter((v) => v.age >= 20 && v.age < 30)
+    .sort((a, b) => a.age < b.age)
+    .map((v) => v.email);
 }
 
 // export를 수정하지 마세요.
